b9078f9217ffa618ddf25b943bbce1e27d2789d4
Eric Paul <ericallenpaul@hotmail.com>
2026-06-08 16:21:36 -0400

    chore: clean up react tooling skeleton

--- src/alertblurty.Web/src/main.tsx
+++ src/alertblurty.Web/src/main.tsx
@@ -1,17 +1,17 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom';
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import 'bootstrap-icons/font/bootstrap-icons.css';
-import './styles/site.css';
-import './styles/alertyblurty.css';
-import { App } from './App';
-import { AuthProvider } from './auth/AuthProvider';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap-icons/font/bootstrap-icons.css";
+import "./styles/site.css";
+import "./styles/alertyblurty.css";
+import { App } from "./App";
+import { AuthProvider } from "./auth/AuthProvider";
 
 const queryClient = new QueryClient();
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <QueryClientProvider client={queryClient}>
       <BrowserRouter>
